f1cc44278a01eea835321acd4b0c10dd920ff473
theoleonet <theo.leonet@student.hepl.be>
2022-08-07 10:59:22 +0200

    :tada: Initial commit

---
+++ src/MakeSrcset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TheoLeonet;
+
+class SrcsetMaker
+{
+	public function __construct(string $image_path, array $sizes) {
+		$this->make_srcset($image_path, $sizes);
+	}
+
+	protected function make_srcset(string $image_path, array $sizes): void
+	{
+		$srcset = '';
+		$image_infos = pathinfo($image_path);
+		foreach ($sizes as $width => $height) {
+			$srcset .= $image_infos['dirname'] . '/' . $image_infos['filename'] . "_${width}" . "_${height}." . $image_infos['extension'] . " ${width}w,<br>";
+		}
+		echo rtrim($srcset, ",");
+	}
+}
